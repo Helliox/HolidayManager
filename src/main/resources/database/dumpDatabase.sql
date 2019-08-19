51f0b92586aa1158c9eb9ad7b737a17e5582d4d3
praktyki <heliox8888@yahoo.pl>
2019-08-19 15:06:22 +0200

    Schemat bazy

--- src/main/resources/database/dumpDatabase.sql
+++ src/main/resources/database/dumpDatabase.sql
@@ -23,14 +23,14 @@ DROP TABLE IF EXISTS `employee`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `employee` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_employee` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `surname` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
-  `manager_status` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `manager_status` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_employee`),
+  UNIQUE KEY `id_UNIQUE` (`id_employee`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +39,7 @@ CREATE TABLE `employee` (
 
 LOCK TABLES `employee` WRITE;
 /*!40000 ALTER TABLE `employee` DISABLE KEYS */;
-INSERT INTO `employee` VALUES (1,'Jan','Kowalski','jan.kowalski@email.pl',0),(2,'Mariusz','Jakimiuk','m.j@email.pl',0);
+INSERT INTO `employee` VALUES (1,'Janusz','Kowalski','jan.kowalski@email.pl',0),(3,'Piotr','Gryc','email@email.pl',0),(4,'Jan','Pawel','jp@mail.com',0);
 /*!40000 ALTER TABLE `employee` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -55,8 +55,8 @@ CREATE TABLE `employee_expertise` (
   `id_expertise` int(11) NOT NULL,
   KEY `fk_employee_expertise_1_idx` (`id_employee`),
   KEY `fk_employee_expertise_2_idx` (`id_expertise`),
-  CONSTRAINT `id_employee` FOREIGN KEY (`id_employee`) REFERENCES `employee` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `id_expertise` FOREIGN KEY (`id_expertise`) REFERENCES `expertise` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `id_employee` FOREIGN KEY (`id_employee`) REFERENCES `employee` (`id_employee`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `id_expertise` FOREIGN KEY (`id_expertise`) REFERENCES `expertise` (`id_expertise`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -66,6 +66,7 @@ CREATE TABLE `employee_expertise` (
 
 LOCK TABLES `employee_expertise` WRITE;
 /*!40000 ALTER TABLE `employee_expertise` DISABLE KEYS */;
+INSERT INTO `employee_expertise` VALUES (1,1);
 /*!40000 ALTER TABLE `employee_expertise` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -77,11 +78,11 @@ DROP TABLE IF EXISTS `expertise`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `expertise` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id_expertise` int(11) NOT NULL AUTO_INCREMENT,
+  `expertise_type` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_expertise`),
+  UNIQUE KEY `id_UNIQUE` (`id_expertise`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -90,6 +91,7 @@ CREATE TABLE `expertise` (
 
 LOCK TABLES `expertise` WRITE;
 /*!40000 ALTER TABLE `expertise` DISABLE KEYS */;
+INSERT INTO `expertise` VALUES (1,'programista');
 /*!40000 ALTER TABLE `expertise` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -101,15 +103,15 @@ DROP TABLE IF EXISTS `holiday`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `holiday` (
-  `id` int(11) NOT NULL,
+  `id_holiday` int(11) NOT NULL AUTO_INCREMENT,
   `date_start` datetime NOT NULL,
   `date_end` varchar(45) NOT NULL,
-  `acceptation` int(11) NOT NULL,
+  `acceptation` int(11) NOT NULL DEFAULT '0',
   `id_employee` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
+  PRIMARY KEY (`id_holiday`),
+  UNIQUE KEY `id_UNIQUE` (`id_holiday`),
   KEY `fk_new_table_1_idx` (`id_employee`),
-  CONSTRAINT `fk_new_table_1` FOREIGN KEY (`id_employee`) REFERENCES `employee` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_new_table_1` FOREIGN KEY (`id_employee`) REFERENCES `employee` (`id_employee`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -130,15 +132,15 @@ DROP TABLE IF EXISTS `overtime`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `overtime` (
-  `id` int(11) NOT NULL,
+  `id_overtime` int(11) NOT NULL AUTO_INCREMENT,
   `date_start` datetime NOT NULL,
   `date_end` datetime NOT NULL,
   `id_employee` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
+  PRIMARY KEY (`id_overtime`),
+  UNIQUE KEY `id_UNIQUE` (`id_overtime`),
   KEY `fk_overtime_1_idx` (`id_employee`),
-  CONSTRAINT `fk_overtime_1` FOREIGN KEY (`id_employee`) REFERENCES `employee` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  CONSTRAINT `fk_overtime_1` FOREIGN KEY (`id_employee`) REFERENCES `employee` (`id_employee`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -147,6 +149,7 @@ CREATE TABLE `overtime` (
 
 LOCK TABLES `overtime` WRITE;
 /*!40000 ALTER TABLE `overtime` DISABLE KEYS */;
+INSERT INTO `overtime` VALUES (1,'2019-08-12 16:00:00','2019-08-12 18:00:00',1);
 /*!40000 ALTER TABLE `overtime` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -158,15 +161,15 @@ DROP TABLE IF EXISTS `used_overtime`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `used_overtime` (
-  `id` int(11) NOT NULL,
+  `id_usedovertime` int(11) NOT NULL AUTO_INCREMENT,
   `date_start` datetime NOT NULL,
   `date_end` datetime NOT NULL,
-  `acceptation` int(11) NOT NULL,
+  `acceptation` int(11) NOT NULL DEFAULT '0',
   `id_employee` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
+  PRIMARY KEY (`id_usedovertime`),
+  UNIQUE KEY `id_UNIQUE` (`id_usedovertime`),
   KEY `fk_used_overtime_1_idx` (`id_employee`),
-  CONSTRAINT `fk_used_overtime_1` FOREIGN KEY (`id_employee`) REFERENCES `employee` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_used_overtime_1` FOREIGN KEY (`id_employee`) REFERENCES `employee` (`id_employee`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -188,4 +191,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-08-14 14:32:29
+-- Dump completed on 2019-08-19 15:04:48
